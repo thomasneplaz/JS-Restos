4f9d5752d808ec6b236887cd36cfabf75e44c6fa
admin <admin@iMac3-47.local>
2018-05-14 17:17:26 +0200

    add tab gestion

---
+++ js/viewHelper.js
@@ -0,0 +1,13 @@
+function viewhelper() {
+    this.animateTab = function() {
+        debugger
+    }
+    this.showTab = function(tab) {
+        let tabName = tab.dataset.link;
+        let sections = document.querySelectorAll('[data-view]');
+        for (var i = 0; i < sections.length: i++){
+            sections[i].classList.add('hidden');
+        }
+        document.querySelector('[data-view="' + tabName + '"]').classList.remove('hidden');
+    }
+}
--- renderer.js
+++ renderer.js
@@ -1,3 +1,11 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+let viewHelper = new ViewHelper();
+
+document.querySelector('aside ul').addEventListener('click', function(el) {
+    //If el.target is a li do somathing
+    if(ek.target.nodeName == 'LI') {
+        ViewHelper.showTab(el.target);
+    }
+})
