8fa4ea9a74acf83c2a1ee62e745cf03cd7ff2e79
admin <admin@iMac3-47.local>
2018-05-15 14:41:56 +0200

    mise à jour js

--- renderer.js
+++ renderer.js
@@ -17,3 +17,75 @@ document.querySelector('section ul').addEventListener('click', function(el) {
         ViewGestion.showTab(el.target);
     }
 })
+
+
+
+//initialise le localStorage
+function initialiserRestos ()
+{
+  if (localStorage.getItem("restos") === null)
+  {
+    // Si la "BDD" localStorage n'existe pas, on l'initialise //
+    var restos = [];
+    // Sauvegarder en localStorage //
+    localStorage['restos'] = JSON.stringify(restos);
+  }
+
+//récupère une liste de restos
+function getRestos ()
+{
+  // Charger en localStorage //
+  var restos = JSON.parse(localStorage['restos']);
+  // Retourner la liste //
+  return restos;
+}
+
+function creerListeRestos (lesRestos)
+{
+  html = '<ul>';
+
+  // Parcourir //
+  lesRestos.forEach(function(item, index, array)
+  {
+    html += '<li>' + item['nom'] + '</li>';
+    html += '<ul><li>' + item['description'] + '</li></ul>';
+  });
+
+  html += '</ul>'
+
+  // Retourner la liste //
+  return html;
+}
+
+//fonction pour ajouter un resto avec nom et description en format html
+function ajouterResto (nomResto, descriptionResto)
+{
+  // Charger en localStorage //
+  var restos = JSON.parse(localStorage['restos']);
+  // Ajouter l'objet //
+  var leResto = new Object();
+  leResto["nom"] = nomResto;
+  leResto["description"] = descriptionResto;
+
+  var newLength = restos.push(leResto);
+  // Sauvegarder en localStorage //
+  localStorage['restos'] = JSON.stringify(restos);
+
+  //alert('Ajout OK');
+}
+
+function creerListeRestosCheckbox (lesRestos)
+{
+  html = '<ul>';
+
+  // Parcourir //
+  lesRestos.forEach(function(item, index, array)
+  {
+    html += '<li>' + item['nom'] + '</li>';
+  });
+
+  html += '</ul>'
+
+  // Retourner la liste //
+  return html;
+}
