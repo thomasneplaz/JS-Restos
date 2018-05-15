62ab7ae0abda016ce70d0758d51bb7c6644935c8
admin <admin@iMac3-47.local>
2018-05-15 12:19:26 +0200

    dossier de dev

--- js/viewHelper.js
+++ js/viewHelper.js
@@ -1,13 +1,27 @@
-function viewhelper() {
+function viewHelper() {
     this.animateTab = function() {
-        debugger
+        debugger;
     }
     this.showTab = function(tab) {
         let tabName = tab.dataset.link;
         let sections = document.querySelectorAll('[data-view]');
-        for (var i = 0; i < sections.length: i++){
+        for (var i = 0; i < sections.length; i++){
             sections[i].classList.add('hidden');
         }
         document.querySelector('[data-view="' + tabName + '"]').classList.remove('hidden');
+    }
+}
+
+function viewGestion() {
+    this.animateTab = function() {
+        debugger;
+    }
+    this.showTab = function(tab) {
+        let tabName = tab.dataset.link;
+        let sections = document.querySelectorAll('[data-form]');
+        for (var i = 0; i < sections.length; i++){
+            sections[i].classList.add('hidden');
+        }
+        document.querySelector('[data-form="' + tabName + '"]').classList.remove('hidden');
     }
 }
--- renderer.js
+++ renderer.js
@@ -1,11 +1,19 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-let viewHelper = new ViewHelper();
+let ViewHelper = new viewHelper();
 
 document.querySelector('aside ul').addEventListener('click', function(el) {
     //If el.target is a li do somathing
-    if(ek.target.nodeName == 'LI') {
+    if(el.target.nodeName == 'LI') {
         ViewHelper.showTab(el.target);
+    }
+})
+
+let ViewGestion = new viewGestion();
+
+document.querySelector('section ul').addEventListener('click', function(el) {
+    if(el.target.nodeName == 'LI'){
+        ViewGestion.showTab(el.target);
     }
 })
